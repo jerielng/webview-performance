fc2d1e691f00c6bae62db54f33655543805b4d8d
Jeriel Ng <jeriel.ng@braze.com>
2022-11-30 17:00:58 -0500

    Initial Commit

---
+++ WebViewPerformance/WebViewPerformanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewPerformanceApp.swift
+//  WebViewPerformance
+//
+//  Created by Jeriel Ng on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct WebViewPerformanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
